cbc64551aa4fc53272cfa1386ef356e56ef6b71a
lwp <23682486@qq.com>
2023-06-23 14:51:24 +0800

    添加c#控制台程序，和堆排序、KMP算法实现

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// using DataStructure.HeapSort;
+using DataStructure.KMP;
+
+// See https://aka.ms/new-console-template for more information
+
+// HeapSort
+// int[] arr = new int[]{ 4, 10, 3, 8, 1, 5 };
+// HeapSortProgram.HeapSort(arr, arr.Length);
+// HeapSortProgram.PrintArr(arr);
+
+//KMP
+string heystack = "aabaacaabaaf";
+string needle = "aabaaf";
+int result = KMPProgram.strStr(heystack, needle);
+
+Console.WriteLine("Hello, World!");
+
